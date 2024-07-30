f00efb637af8e787c84ad831d4bacc9a4f3ce348
Brenden Bilson <brendenbilson5@gmail.com>
2024-07-30 16:19:26 -0700

    Create Vulkan instance with debugging

--- src/renderApp.cpp
+++ src/renderApp.cpp
@@ -2,30 +2,22 @@
 
 void renderApp::initWindow()
 {
-    SDL_Surface* surface = NULL;
-
     //If you cannot intialize SDL2, exit with error code    
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "Could not initialize SDL! SDL_Error: " << SDL_GetError() << "\n";
-        throw EXIT_FAILURE;
-    }
+        throw std::runtime_error(("Could not initialize SDL! SDL_Error: %s\n", SDL_GetError()));
 
     window = SDL_CreateWindow("Vulkan Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_VULKAN);
 
     //If you cannot create the window, exit with error code    
     if (window == NULL)
-    {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
-        throw EXIT_FAILURE;
-    }
+        throw std::runtime_error(("Window could not be created! SDL_Error: %s\n", SDL_GetError()));
 
     SDL_SetWindowResizable(window, SDL_FALSE);
 }
 
 void renderApp::initVulkan()
 {
-
+    createInstance();
 }
 
 void renderApp::loop()
@@ -44,8 +36,49 @@ void renderApp::loop()
 
 void renderApp::clean()
 {
+    vkDestroyInstance(instance, nullptr);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void renderApp::createInstance()
+{
+    //Fill struct with application info (optional)
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Rubik Rescue";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    //Fill struct with instance info (requried)
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    //Get the names of Vulkan instance extensions required to create an SDL Vulkan Surface
+    uint32_t sdlExtensionCount = 0;
+    SDL_Vulkan_GetInstanceExtensions(window, &sdlExtensionCount, nullptr);
+    std::vector<const char*> sdlExtensions(sdlExtensionCount);
+    SDL_Vulkan_GetInstanceExtensions(window, &sdlExtensionCount, sdlExtensions.data());
+    createInfo.ppEnabledExtensionNames = sdlExtensions.data();
+    createInfo.enabledExtensionCount = sdlExtensions.size();
+    createInfo.enabledLayerCount = 0;
+
+    //Create the Vulkan instance with all of the information we acquired and check if successfull
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error(("Failed to create Vulkan instance! Vulkan Result: %s\n", string_VkResult(result)));
+
+    //Retrieve list of extensions supported by Vulkan
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+    std::cout << "Available Supported Extensions:\n";
+    for (const auto& extension : extensions)
+        std::cout << '\t' << extension.extensionName << '\n';
 }
 
 void renderApp::run()
--- src/renderApp.h
+++ src/renderApp.h
@@ -4,6 +4,8 @@
 #include <SDL_vulkan.h>
 #include <glm/glm.hpp>
 #include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vector>
 
 const uint32_t SCREEN_WIDTH = 1080;
 const uint32_t SCREEN_HEIGHT = 720;
@@ -12,11 +14,14 @@ class renderApp
 {
 private:
     SDL_Window* window = NULL;
+    VkInstance instance;
 
     void initWindow();
     void initVulkan();
     void loop();
     void clean();
+
+    void createInstance();
 public:
     void run();
 };
